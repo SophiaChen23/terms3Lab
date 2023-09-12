c6ffc467e385d733796788b9fb559dbeee5580ae
sophia <zchen9523@gmail.com>
2023-09-12 18:27:36 -0500

    w2d1lab

---
+++ demo2.js
@@ -0,0 +1,34 @@
+class Reservation extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          isGoing: true,
+          numberOfGuests: 2
+      };
+
+      this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ?
+            target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+      return (
+        <form>
+
+        </form>
+      );
+    }
+  }
+
+  ReactDOM.render(
+    <Reservation />,
+    document.getElementById('root')
+  );
